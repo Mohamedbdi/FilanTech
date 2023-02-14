60cadcbf3a300260105f33fefdd0adb14cede772
moha_hareeri <mcabdi578@gmail.com>
2023-02-14 04:42:46 -0900

    greeting message for that particular name

---
+++ DSA_Assigniment2_02/src/dsa_assigniment2_02/DSA_Assigniment2_02.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsa_assigniment2_02;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Moha_Hareeri
+ */
+public class DSA_Assigniment2_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name:  ");
+        String name = in.nextLine();
+        System.out.println("hello :  "+name);
+    }
+    
+}
